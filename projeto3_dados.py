10fffaad70d39909be39fa39dcc918c05b9304d9
guilbd <57806160+guilbd@users.noreply.github.com>
2021-06-27 07:24:14 -0300

    Atualização
    Inclusão da lista de jogadas ordenadas. Visualizada ao final das rodadas, na opção LOG de jogadas.

--- projeto3_dados.py
+++ projeto3_dados.py
@@ -1,3 +1,9 @@
+# O programa:
+# • Pergunta quantas rodadas você quer fazer; 
+# • Guardar os resultados dos dados em um dicionário. 
+# • Ordenar esse dicionário, sabendo que o vencedor tirou o maior número no dado.
+# • Mostra  no  final  qual  jogador  ganhou  mais  rodadas  e  foi  o  grande campeão.
+
 from random import randint #Biblioteca para gerar os números dos dados aleatórios
 import operator #Biblioteca para ordenar os dicionarios por ordem de Valor das Chaves
 from time import sleep
@@ -46,9 +52,9 @@ linhas()
 dadosResult = list() # criação das listas para receber os dicionários
 
 vitPlayer1 = vitPlayer2 = vitPlayer3 = vitPlayer4 = 0 # Contadores para os resultados das rodadas
-print('ROLANDO OS DADOS...')
+print('[yellow]ROLANDO OS DADOS...')
 print()
-for i in tqdm(range(1000)):
+for i in tqdm(range(100)): # Barra de progresso lib TDQM para decoração do jogo
     sleep(0.001)
 print()
 for r in range(rodadas):
@@ -59,9 +65,9 @@ for r in range(rodadas):
     rodada['player3'] = randint(1, 6)
     rodada['player4'] = randint(1, 6)
     sleep(1)
-    print(rodada)
+    print(f"{r + 1}º rodada: {rodada}") # Visualização das jogadas em tempo real
     sleep(1)
-    if (rodada['player3'] < rodada['player1'] > rodada['player2']) and rodada['player1'] > rodada['player4'] :
+    if (rodada['player3'] < rodada['player1'] > rodada['player2']) and rodada['player1'] > rodada['player4'] : # Inclusão das vitórias dos jogadores
         vitPlayer1+=1
     elif (rodada['player3'] < rodada['player2'] > rodada['player1']) and rodada['player2'] > rodada['player4'] :
         vitPlayer2+=1
@@ -75,7 +81,7 @@ for r in range(rodadas):
     dadosResult.append(sortedResult) # Cada dicionário temporário criado é adicionado ao dadosResult que contem todos as rodadas
 sleep(1)
 linhas()
-if (vitPlayer2 < vitPlayer1 > vitPlayer3) and vitPlayer1 > vitPlayer4:
+if (vitPlayer2 < vitPlayer1 > vitPlayer3) and vitPlayer1 > vitPlayer4: # Estrutura condicional para demonstrar quem ganhou mais rodadas
     print(f'O GRANDE CAMPEÃO É O JOGADOR 1 COM {vitPlayer1} VITÓRIAS.')
 elif (vitPlayer3 < vitPlayer2 > vitPlayer1) and vitPlayer2 > vitPlayer4:
     print(f'O GRANDE CAMPEÃO É O JOGADOR 2 COM {vitPlayer2} VITÓRIAS.')
@@ -90,7 +96,6 @@ linhas()
 sleep(1)
 print(f'{"[bold yellow]ESTATÍSTICAS DO JOGO[/bold yellow]":^30}')
 linhas()
-#print(dadosResult)
 print(f'''
 JOGADOR 1 = {vitPlayer1} VITÓRIAS
 JOGADOR 2 = {vitPlayer2} VITÓRIAS
@@ -98,3 +103,12 @@ JOGADOR 3 = {vitPlayer3} VITÓRIAS
 JOGADOR 4 = {vitPlayer4} VITÓRIAS
 EMPATES = {empates}
 ''')
+
+log = input(f'Deseja verificar o histórico de jogadas? [S/N] ').upper().strip()[0] # Exbibição da lista com todas as rodadas
+if log == 'S':
+    print(f'[bold red]LOG de jogadas para conferência: {dadosResult}')
+    print('OBRIGADO')
+    
+else:
+    print('OBRIGADO')
+    
